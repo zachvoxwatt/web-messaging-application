164bdc64db0487c6ba80d2a0f56be1c062356324
Võ C. Kha <53788250+zachvoxwatt@users.noreply.github.com>
2022-06-06 23:12:11 +0700

    v0.0.1
    - Added skeletal structure
    - Added a basic Socket.IO server

--- back-end/routes/PostText.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const processor = require('../controllers/PostText')
-
-router.post('/', processor.postText)
-
-module.exports = router
--- back-end/server.js
+++ back-end/server.js
@@ -1,32 +1,18 @@
-// Server resources initialization
-const logger = require('./controllers/utils/EventLogger')
+// Loads all of the configs
+require('dotenv').config()
+const svCfg = process.env
+const socketIOUtils = require('./utils/socketIO')
+
+// Initialize server resources
 const express = require('express')
-const cors = require('cors')
-const https = require('https')
-const { Server } = require('socket.io')
 const expressServer = express()
-const socketIOServer = new Server(https.createServer(expressServer))
-const db = require('./controllers/utils/Database')
+const httpServer = require('http').Server(expressServer)
+const socketIOServer = socketIOUtils.prepareServer(httpServer)
+socketIOUtils.connection(socketIOServer)
 
-// Load the config JSON
-const svcfg = require('./server-cfg')
-
-// Route declarations
-const loginRoute = require('./routes/Login')
-const registerRoute = require('./routes/Registration')
-const postTextRoute = require('./routes/PostText')
-
-// Middleware declarations
-expressServer.use(cors())
-expressServer.use(express.urlencoded({extended: false}))
+// Initialize express middlewares
+expressServer.use(express.urlencoded({ extended: false }))
 expressServer.use(express.json())
 
-// Bind routes to the declared ones
-expressServer.use('/login', loginRoute)
-expressServer.use('/register', registerRoute)
-expressServer.use('/postText', postTextRoute)
-
-// Run the server
-expressServer.listen(svcfg.SERVER_PORT, (e) => { db.testConnection(); logger(`Server is operational at port ${svcfg.SERVER_PORT}`)} )
-
-socketIOServer.on('connection', (socket) => { console.log('A client has connected!') })
+// Runs the server
+httpServer.listen(svCfg.SERVER_PORT, () => { console.log(`Server is operational on port ${svCfg.SERVER_PORT}`) })
