fb32fc439d8ef981fe49cc99cafc44a0bd45d781
ZeDamien2K <53788250+zachvoxwatt@users.noreply.github.com>
2022-06-08 06:34:51 +0700

    v0.0.1c
    - Deleted front-end directory

--- front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RunnyApplication from './scripts/_RunnyApplication'
-import { BrowserRouter } from 'react-router-dom'
-import './css/index.css'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><BrowserRouter><RunnyApplication /></BrowserRouter></React.StrictMode>);
