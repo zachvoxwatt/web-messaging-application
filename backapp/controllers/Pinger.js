ea6d45c20de65f6cb4fbb4ca412d778f8d9746a2
ZeDamien2K <53788250+zachvoxwatt@users.noreply.github.com>
2022-06-20 01:40:29 +0700

    Hotfix
    - Readded 'public' folder in frontapp

--- backapp/controllers/Pinger.js
+++
@@ -1,1 +0,0 @@
-exports.pinger = async (req, res, next) => { res.status(200).send({ isOnline: true }) }
--- backapp/routes/pingRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const processor = require('../controllers/Pinger')
-
-router.get('/', processor.pinger)
-module.exports = router
